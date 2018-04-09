4f3083bb2727fb5cbda1f9f6a70527d67cc332b3
gangeshraj <30901825+gangeshraj@users.noreply.github.com>
2018-04-09 13:42:41 +0530

    Done the logic of removing sidedrawer
    (but backdrawer.js has to change name of variable to props.showstatus)

--- src/Components/UI/Navigation/Sidedrawer/Sidedrawer.js
+++ src/Components/UI/Navigation/Sidedrawer/Sidedrawer.js
@@ -6,10 +6,16 @@ import Backdrop from '../../Backdrop/Backdrop';
 import Auxillary from '../../../../higherordercomponent/Auxillary';
 
 const sidedrawer=(props)=>{
+    let attachedClasses=[Classes.Sidedrawer,Classes.Close];
+
+    if(props.showstatus===true){
+        attachedClasses=[Classes.Sidedrawer,Classes.Open]
+    }
+    
     return (
         <Auxillary>
-            <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={Classes.Sidedrawer}>
+            <Backdrop showstatus={props.showstatus} clicked={props.closed}/>
+            <div className={attachedClasses.join(" ")}>
                 <Logo height="11%"/>
                 <nav>
                     <Navigationitems/>

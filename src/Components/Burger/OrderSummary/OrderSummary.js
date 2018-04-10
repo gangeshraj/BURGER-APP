7978be71305170e8acfda164d41c810f09372382
gangeshraj <30901825+gangeshraj@users.noreply.github.com>
2018-04-10 11:21:05 +0530

    orderSummary now is class component with one lifecycle hook

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Auxillary from '../../../higherordercomponent/Auxillary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary=(props)=>{
+class OrderSummary extends Component{
+    
+    
+    componentWillUpdate(){
+        //this componet will update only when the parent Modal component
+        //"show" props changes as shouldComponentUpdate checks it
+        //for updating
+        console.log("order summaryt will update");
+    }
 
-    const ingredientSummary=Object.keys(props.ingredients)
-                            .map(igkey=>(
-                                <li key={igkey}>
-                                    <span style={{textTransform:'capitalize'}}>{igkey}</span>
-                                    :{props.ingredients[igkey]}
-                                </li>   )
-                                )
 
-    return <Auxillary>
-                <h3>Your order</h3>
-                <p>A delicious burger with the following ingredients</p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p><strong>Total Price: {props.price.toFixed(2)} </strong></p>
-                <p>Continue to check out?</p>
-                <Button btnType="Danger" clicked={props.purchaseCancel}>CANCEL</Button>
-                <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
-           </Auxillary>;
+    render(){
+        
+                    const ingredientSummary=Object.keys(this.props.ingredients).map(igkey=>(
+                    <li key={igkey}>
+                        <span style={{textTransform:'capitalize'}}>{igkey}</span>
+                        :{this.props.ingredients[igkey]}
+                    </li>   )
+                     )
+        
+        
+        
+            return <Auxillary>
+                        <h3>Your order</h3>
+                        <p>A delicious burger with the following ingredients</p>
+                        <ul>
+                            {ingredientSummary}
+                        </ul>
+                        <p><strong>Total Price: {this.props.price.toFixed(2)} </strong></p>
+                        <p>Continue to check out?</p>
+                        <Button btnType="Danger" clicked={this.props.purchaseCancel}>CANCEL</Button>
+                        <Button btnType="Success" clicked={this.props.purchaseContinue}>Continue</Button>
+                   </Auxillary>;
+            }
 
 }
 export default OrderSummary;

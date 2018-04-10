6f41d7cce7e3fb6f8a710ed9de9081142bd6689c
gangeshraj <30901825+gangeshraj@users.noreply.github.com>
2018-04-10 11:06:28 +0530

    Improvement of performance
    modal component changed to class component from functional component enhanced performance.As it should update only when the modal is shown therefore shouldComponentUpdate is used and obviously the child component order summary is not updated

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Classes from './Modal.css';
 import Auxillary from '../../../higherordercomponent/Auxillary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal=(props)=>(
-    <Auxillary>
-    <Backdrop show={props.show} clicked={props.closeModal}/>
-    <div 
-        className={Classes.Modal}
-        style={{
-            transform:props.show? 'translateY(0)':'translateY(-100vh)',
-            opacity:props.show?'1':'0'
-        }}
-    >
-        {props.children}
-    </div>
-    </Auxillary>
-)
+class Modal extends Component{
+    
+    shouldComponentUpdate(nextProps,prevState){//if this modal is shown han only we need 
+        //to update it and obviously child component of modal orderSummary component 
+        //therefore it enhances performance
+        return nextProp.show!==this.props.show;
+    }
+    
+    render(){
+     return (
+        <Auxillary>
+        <Backdrop show={this.props.show} clicked={this.props.closeModal}/>
+        <div 
+            className={Classes.Modal}
+            style={{
+                transform:this.props.show? 'translateY(0)':'translateY(-100vh)',
+                opacity:this.props.show?'1':'0'
+            }}
+        >
+            {this.props.children}
+        </div>
+        </Auxillary>
+         );
+    }
+}
 
 export default Modal;
 
